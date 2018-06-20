162ba36e21fc67fa4e0cd3c30c97a84ca6f77e1c
Octavian Dima <hello@wopss.ro>
2018-06-20 12:00:57 +0200

    Add login design

--- Source/app/src/main/java/nl/saxion/act/i7/quitter/activities/LoginActivity.java
+++ Source/app/src/main/java/nl/saxion/act/i7/quitter/activities/LoginActivity.java
@@ -2,10 +2,15 @@ package nl.saxion.act.i7.quitter.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import nl.saxion.act.i7.quitter.R;
 
 public class LoginActivity extends AppCompatActivity {
+    public void onLoginClick(View view) {
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
