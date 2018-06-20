3cc232815563e071651cbb3ba65e3f33ebf14e13
Octavian Dima <hello@wopss.ro>
2018-06-20 08:23:06 +0200

    Add login activity

--- Source/app/src/main/java/nl/saxion/act/i7/quitter/MainActivity.java
+++ Source/app/src/main/java/nl/saxion/act/i7/quitter/MainActivity.java
@@ -1,19 +1,10 @@
 package nl.saxion.act.i7.quitter;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.support.v7.app.AppCompatActivity;
 
-import org.json.JSONObject;
-
-import java.io.InputStream;
-
-import nl.saxion.act.i7.quitter.activities.UserDetailedActivity;
-import nl.saxion.act.i7.quitter.data_adapters.TweetDataAdapter;
-import nl.saxion.act.i7.quitter.data_providers.TweetDataProvider;
+import nl.saxion.act.i7.quitter.activities.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,31 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        InputStream stream = this.getBaseContext().getResources().openRawResource(R.raw.tweets);
+        Intent activityIntent;
 
-        try {
-            byte[] bytes = new byte[stream.available()];
-            stream.read(bytes);
+        activityIntent = new Intent(this, LoginActivity.class);
 
-            String fileContent = new String(bytes);
-            TweetDataProvider.reload(new JSONObject(fileContent));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        TweetDataAdapter tweetDataAdapter = new TweetDataAdapter(this, TweetDataProvider.getAll());
-
-        ListView tweetList = this.findViewById(R.id.tweetList);
-        tweetList.setAdapter(tweetDataAdapter);
-
-        tweetList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this, UserDetailedActivity.class);
-                intent.putExtra("index", position);
-
-                startActivity(intent);
-            }
-        });
+        startActivity(activityIntent);
+        finish();
     }
 }
---
+++ Source/app/src/main/java/nl/saxion/act/i7/quitter/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package nl.saxion.act.i7.quitter.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import nl.saxion.act.i7.quitter.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
