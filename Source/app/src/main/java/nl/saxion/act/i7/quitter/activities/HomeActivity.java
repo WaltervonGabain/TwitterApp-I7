fe8faaf299bd42c00649a05d85ff2b140a7d3a65
Octavian Dima <hello@wopss.ro>
2018-06-20 20:40:29 +0200

    Remove login activity

--- Source/app/src/main/java/nl/saxion/act/i7/quitter/activities/HomeActivity.java
+++ Source/app/src/main/java/nl/saxion/act/i7/quitter/activities/HomeActivity.java
@@ -20,32 +20,5 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        InputStream stream = this.getBaseContext().getResources().openRawResource(R.raw.tweets);
-
-        try {
-            byte[] bytes = new byte[stream.available()];
-            stream.read(bytes);
-
-            String fileContent = new String(bytes);
-            TweetDataProvider.reload(new JSONObject(fileContent));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        TweetDataAdapter tweetDataAdapter = new TweetDataAdapter(this, TweetDataProvider.getAll());
-
-        ListView tweetList = this.findViewById(R.id.tweetList);
-        tweetList.setAdapter(tweetDataAdapter);
-
-        tweetList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(HomeActivity.this, UserDetailedActivity.class);
-                intent.putExtra("index", position);
-
-                startActivity(intent);
-            }
-        });
     }
 }
