431c825a455ac43824ebd135509e4cb94c5293b2
Octavian Dima <hello@wopss.ro>
2018-06-20 20:48:47 +0200

    Remove unused imports

--- Source/app/src/main/java/nl/saxion/act/i7/quitter/activities/HomeActivity.java
+++ Source/app/src/main/java/nl/saxion/act/i7/quitter/activities/HomeActivity.java
@@ -1,19 +1,9 @@
 package nl.saxion.act.i7.quitter.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
-import org.json.JSONObject;
-
-import java.io.InputStream;
 
 import nl.saxion.act.i7.quitter.R;
-import nl.saxion.act.i7.quitter.data_adapters.TweetDataAdapter;
-import nl.saxion.act.i7.quitter.data_providers.TweetDataProvider;
 
 public class HomeActivity extends AppCompatActivity {
     @Override
